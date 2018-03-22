3d3dfc66a66b07f2e35220faaf390e70b2a0d48e
ccantarero <c.cantarero91@outlook.es>
2018-03-22 01:08:06 +0100

    all works

--- src/main/scala/patmat/Huffman.scala
+++ src/main/scala/patmat/Huffman.scala
@@ -229,7 +229,8 @@ object Huffman {
    * This function returns the bit sequence that represents the character `char` in
    * the code table `table`.
    */
-    def codeBits(table: CodeTable)(char: Char): List[Bit] = table.filter(_._1 == Char).head._2
+    def codeBits(table: CodeTable)(char: Char): List[Bit] = table.find(a => a._1 == char)
+      .getOrElse(throw new Error(s"codeBits for a char that doesn't exist. char = $char ; table = $table"))._2
 
   /**
    * Given a code tree, create a code table which contains, for every character in the
@@ -239,14 +240,24 @@ object Huffman {
    * a valid code tree that can be represented as a code table. Using the code tables of the
    * sub-trees, think of how to build the code table for the entire tree.
    */
-    def convert(tree: CodeTree): CodeTable = ???
+    def convert(tree: CodeTree): CodeTable = {
+      def converAcc(currentTree: CodeTree, currentBits: List[Bit]): CodeTable = {
+        currentTree match {
+          case f : Fork => mergeCodeTables(converAcc(f.left,0 :: currentBits), converAcc(f.right,1 :: currentBits))
+          case l : Leaf => List((l.char,currentBits))
+        }
+      }
+      converAcc(tree, List())
+    }
   
   /**
    * This function takes two code tables and merges them into one. Depending on how you
    * use it in the `convert` method above, this merge method might also do some transformations
    * on the two parameter code tables.
    */
-    def mergeCodeTables(a: CodeTable, b: CodeTable): CodeTable = ???
+    def mergeCodeTables(a: CodeTable, b: CodeTable): CodeTable = {
+      a union b
+    }
   
   /**
    * This function encodes `text` according to the code tree `tree`.
@@ -254,5 +265,7 @@ object Huffman {
    * To speed up the encoding process, it first converts the code tree to a code table
    * and then uses it to perform the actual encoding.
    */
-    def quickEncode(tree: CodeTree)(text: List[Char]): List[Bit] = ???
+    def quickEncode(tree: CodeTree)(text: List[Char]): List[Bit] = {
+      text.foldLeft(List() : List[Bit])((z,i) => z union codeBits(convert(tree))(i) )
+    }
   }
